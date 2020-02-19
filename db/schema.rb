2f4ebb0a3c32a5549cddcab3fcab7340174686ad
Eugene Lemon <eugenelemon@hiddengeniusproject.org>
2020-02-18 23:49:36 -0800

    Add Navigation and Association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_19_063108) do
+ActiveRecord::Schema.define(version: 2020_02_19_073935) do
 
   create_table "plays", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_02_19_063108) do
     t.string "director"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
